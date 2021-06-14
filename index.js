ae9a0ebbcf98ed8578f80617fdba805be826d96a
varya <varvara.mikhailova@gmail.com>
2021-06-14 13:01:27 +0300

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Мы достаем библиотеку из node_modules/{{express}}/index.js
 const express = require('express');
 const products = require('./src/js/products');
+const brands = require('./src/js/brands');
 const bot = require('./src/js/telegraf');
 const getRequestCallMessageTemplate = require('./src/js/telegram/templates/requestCall.js');
 const bodyParser = require('body-parser');
@@ -12,7 +13,7 @@ const requestCalChatId = '-406798742';
 // Получаем базовое приложение
 const app = express();
 
-// bot.launch();
+bot.launch();
 
 // Настраиваем движок отображения
 app.set('view engine', 'pug');
@@ -29,38 +30,110 @@ app.use(bodyParser.urlencoded({
 
 // Говорим, что будет, если постучать в корень сайта
 app.get('/', (req, res) => {
+	const mainPageProducts = products.filter(product => product.id <= 4);
 	res.render(
 		'index', 
 		{
 			title: 'Drondulet',
-			message: 'Купи дрона сегоня, летай уже к вечеру',
+			message: 'Купи дрона сегодня, летай уже к вечеру',
+			products: mainPageProducts
+		}
+	)
+})
+
+app.get('/drones', (req, res) => {
+	res.render(
+		'drones',
+		{
+			pageTitle: 'Все дроны',
 			products
 		}
 	)
 })
 
-app.get('/product', (req, res) => {
-	res.render(
-		'product',
-		{
-			pageTitle: 'Страница продукта'
-		}
+app.get('/api/newRequest', (req, res) => {
+	bot.telegram.sendMessage(
+		requestCalChatId,
+		prepareRequestCallMessage(req.query)
 	)
+	res.redirect('/');
 })
 
-app.get('/api/request-call', (req, res) => {
-	console.log('Запрос', req.query);
+app.post('/api/purchaseRequest', (req, res) => {
+	console.log('Data', req.body);
 
-	
+	const product = products.find(product => product.id == req.body.productId);
+
 	bot.telegram.sendMessage(
-		requestCallChatId(),
-		getRequestCallMessageTemplate(req.query)
-	)
+		requestCalChatId,
+		`
+		_______________
+		Добрый день! Новая заявка!
 
-	// отправить информацию через бота
+		Имя: ${req.body.name}
+		Почта: ${req.body.email}
+		Телефон: ${req.body.phone}
 
-	res.redirect('/');
+		Продукт для покупки:
+		${product.name}
+		Дополнительные лопасти: ${req.body.extraBlades}
+		Дополнительный аккумулятор: ${req.body.extraAccumulator}
 
+		`
+	);
+
+	res.json('status', 200);
+})
+
+app.get('/:brand', (req, res) => {
+	const brandProducts = products
+		.filter(product => product.brand.toLowerCase() === req.params.brand.toLowerCase());
+	console.log(brandProducts);
+
+	if (brandProducts.length) {
+	const brandInfo = brands.find(brand => brand.name.toLowerCase() === req.params.brand.toLowerCase());
+
+		res.render(
+			'brand',
+			{
+				pageTitle: `Дроны от бренда ${req.params.brand}`,
+				products: brandProducts,
+				brandInfo
+			}
+		)
+	} else {
+		console.log('Тут Фиаско! Такого бренда не существует');
+		res.redirect('/');
+	}
+})
+
+app.get('/:brand/:id', (req, res) => {
+	console.log('brand + id', req.params.brand);
+	const brandProducts = products
+		.filter(product => product.brand.toLowerCase() === req.params.brand.toLowerCase());
+	console.log(brandProducts);
+
+	if (brandProducts.length) {
+		const pageItem = brandProducts.find(product => product.id == req.params.id);
+
+		if (pageItem) {
+			res.render(
+				'product',
+				{
+					pageTitle: `Купить дрон ${pageItem.name}`,
+					brandName: req.params.brand,
+					product: pageItem
+				}
+			)
+		} else {
+			console.log('Тут Фиаско, но бренд есть - посмотри ещё у них');
+			res.redirect('/');
+		}
+
+	} else {
+		console.log('Тут Фиаско! Такого бренда не существует');
+		res.redirect('/');
+	}
 })
 
 // Запуск сервера
--- public/js/productPage.js
+++ public/js/productPage.js
@@ -1,7 +1,61 @@
-console.log('Эй, я кастомный JS только для страницы продукта');
+const buyProductModal = new bootstrap.Modal(document.getElementById('buyProductModal'));
 
-function addToShoppingCard() {
-	console.log(6546546);
-	console.log(getElementById(productTitle));
-	console.log(getElementById(productDefoldPrice));
+function addToCard(product) {
+	console.log(product);
+
+	let finalPrice = product.price - product.discount;
+
+	const extraAccumulator = document.getElementById('extraAccumulator').checked;
+	const extraBlades = document.getElementById('extraBlades').checked;
+
+	if (extraAccumulator) {
+		finalPrice += product.extraAccumulatorPrice;
+	}
+
+	if (extraBlades) {
+		finalPrice += product.extraBladesPrice;
+	}
+
+	console.log(finalPrice);
+
+	document.getElementById('finalPrice').innerText = `${finalPrice}₽`;
+
+	buyProductModal.show();
+}
+
+function closeModal() {
+	buyProductModal.hide();
+}
+
+function sendPurchaseRequest(product) {
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const phone = document.getElementById('phone').value;
+
+	if (!name || !email || !phone) {
+		alert('Заполните все поля, пожалуйста');
+		return;
+	}
+
+	const extraAccumulator = document.getElementById('extraAccumulator').checked;
+	const extraBlades = document.getElementById('extraBlades').checked;
+
+	axios.post(
+		'/api/purchaseRequest',
+		{
+			name,
+			email,
+			phone,
+			productId: product.id,
+			extraAccumulator,
+			extraBlades
+		}
+	)
+	.then(response => {
+		alert('Спасибо за заказ. Мы скоро свяжемся с вами!');
+	})
+	.catch(err => {
+		console.error(err);
+		alert('Просим прощения - какая-то неизвестная ошибка.');
+	})
 }
